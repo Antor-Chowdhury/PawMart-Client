3af204e2c8d05947b0900f5023a4ca48109ec42d
Antor-Chowdhury <antor1347@gmail.com>
2025-12-02 21:33:11 +0600

    added error page

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../root/RootLayout";
+import Error from "../pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout></RootLayout>,
+    errorElement: <Error></Error>,
     children: [],
   },
 ]);
